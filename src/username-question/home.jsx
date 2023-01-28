cd8dc64ee6b3fa7e28cb03c8615ac78192206b0d
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2023-01-28 23:13:20 +0530

    1.3

--- src/username-question/home.jsx
+++ src/username-question/home.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import './home.css'
+import axios from 'axios';
 
 class Home extends Component {
     state = { 
         username : "",
         question : ""
-     } 
+     }
+    
+    handleChangeU=event=>{
+        this.setState({username: event.target.value})
+    }
+
+    handleChangeP=event=>{
+        this.setState({question: event.target.value})
+    }
+
+    handleSubmit=event=>{
+        event.preventDefault(); 
+        console.log(this.state);
+        axios({
+            method:'post',
+            url:'http://127.0.0.1:5000/users/',
+            data:{
+                username: this.state.username,
+                question: this.state.question
+            },
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+    } 
+     
     render() { 
         return (
-                <div className='home-page'>
+            <div className='home-page'>
             <div className='user-form'>
             <h1>Questions Game</h1>
-            <form>
+            <form onSubmit={this.handleSubmit}>
             <div className='username-d'>
                 <div>
                     <span className='username-labl'>@</span>
-                    <input className='username-inp'></input>
+                    <input className='username-inp' type="text" value={this.state.username} onChange={this.handleChangeU}></input>
                 </div>
                 {/* <small>It need not be your real name</small> */}
             </div>
@@ -23,8 +49,9 @@ class Home extends Component {
                 <div>
                     <span className='questions-labl'>Questions:</span>
                 </div>
-                <input className='questions-inp'></input>
+                <textarea className='questions-inp' type="text" value={this.state.question} onChange={this.handleChangeP}></textarea>
             </div>
+            <input id="sub-btn" type="submit" className='btn btn-outline-dark btn-lg' value="submit"/>
             </form>
             </div>
             </div>
